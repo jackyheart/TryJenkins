7223dd1e78dbce4c9b46d5b8ae0858543f09fae1
Jacky Tjoa <jacky@pixerf.com>
2018-04-25 10:33:00 +0800

    first commit

---
+++ TryJenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryJenkins
+//
+//  Created by PIXERF_SG_WS_12 on 25/4/18.
+//  Copyright © 2018 PIXERF_SG_WS_12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
